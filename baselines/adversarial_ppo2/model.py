f837f5b7a8b3efbd93907520d51ff9baa16b29fc
Josh Roy <joshnroy@gmail.com>
2020-03-26 11:17:00 -0400

    uncertainty-based mtl

--- baselines/adversarial_ppo2/model.py
+++ baselines/adversarial_ppo2/model.py
@@ -92,6 +92,9 @@ class Model(object):
             # act_model that is used for sampling
             act_model = policy(nbatch_act, 1, sess)
 
+            self.loss_weight_policy = tf.Variable(-0.346574, name="loss_weight_policy")
+            self.loss_weight_pd = tf.Variable(-0.346574, name="loss_weight_pd")
+
             # Train model for training
             if microbatch_size is None:
                 train_model = policy(nbatch_train, nsteps, sess)
@@ -162,7 +165,9 @@ class Model(object):
         # pd_loss = tf.abs(self.real_labels_loss - self.fake_labels_loss)
         pd_loss = -self.fake_labels_loss
 
-        loss = rl_loss + self.TRAIN_GEN * self.disc_coeff * pd_loss
+        rl_weight = (0.5 / tf.exp(self.loss_weight_policy)**2)
+        pd_weight = (0.5 / tf.exp(self.loss_weight_pd)**2)
+        loss = rl_weight * rl_loss + self.loss_weight_policy #+ self.TRAIN_GEN * pd_weight + self.TRAIN_GEN * self.loss_weight_pd
 
         self.update_discriminator_params(comm, discriminator_loss, mpi_rank_weight, LR, max_grad_norm)
 
@@ -170,9 +175,9 @@ class Model(object):
 
         state_variance = tf.reduce_mean(tf.math.reduce_std(train_model.train_intermediate_feature, axis=0))
 
-        self.loss_names = ['policy_loss', 'value_loss', 'policy_entropy', 'approxkl', 'clipfrac', 'discriminator_loss', 'pd_loss', 'critic_min', 'critic_max', 'real_labels_loss', 'fake_labels_loss', 'state_variance']
+        self.loss_names = ['policy_loss', 'value_loss', 'policy_entropy', 'approxkl', 'clipfrac', 'discriminator_loss', 'pd_loss', 'critic_min', 'critic_max', 'real_labels_loss', 'fake_labels_loss', 'state_variance', 'rl_weight', 'pd_weight']
 
-        self.stats_list = [pg_loss, vf_loss, entropy, approxkl, clipfrac, discriminator_loss, pd_loss, tf.reduce_min(predicted_logits), tf.reduce_max(predicted_logits), self.real_labels_loss, self.fake_labels_loss, state_variance]
+        self.stats_list = [pg_loss, vf_loss, entropy, approxkl, clipfrac, discriminator_loss, pd_loss, tf.reduce_min(predicted_logits), tf.reduce_max(predicted_logits), self.real_labels_loss, self.fake_labels_loss, state_variance, rl_weight, pd_weight]
 
         self.train_model = train_model
         self.act_model = act_model
