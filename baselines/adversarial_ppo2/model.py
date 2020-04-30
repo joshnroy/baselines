c0eff6f7e69f2035d9fc4d97dafb9329c1054bb2
Josh Roy <josh_roy@brown.edu>
2020-04-30 13:50:41 -0400

    memes

--- baselines/adversarial_ppo2/model.py
+++ baselines/adversarial_ppo2/model.py
@@ -32,6 +32,8 @@ def build_discriminator(inputs, num_levels):
 
     out = tf.nn.leaky_relu(tf.layers.dense(out, 512, name='impala_layer_' + get_layer_num_str()), name='impala_layer_' + get_layer_num_str())
     out = tf.nn.leaky_relu(tf.layers.dense(out, 512, name='impala_layer_' + get_layer_num_str()), name='impala_layer_' + get_layer_num_str())
+    # out = tf.nn.leaky_relu(tf.layers.dense(out, 512, name='impala_layer_' + get_layer_num_str()), name='impala_layer_' + get_layer_num_str())
+    # out = tf.nn.leaky_relu(tf.layers.dense(out, 512, name='impala_layer_' + get_layer_num_str()), name='impala_layer_' + get_layer_num_str())
     out = tf.layers.dense(out, 1, name='impala_layer_' + get_layer_num_str())
 
     return out
@@ -92,6 +94,8 @@ class Model(object):
             gp_slopes = tf.sqrt(1e-8 + tf.reduce_sum(tf.square(gp_gradients), 1))
             gp_slopes = tf.debugging.check_numerics(gp_slopes, "Gradient Slope is not a number")
             self.gradient_penalty = tf.reduce_mean((gp_slopes - 1.)**2.)
+            self.gradient_penalty = tf.clip_by_value(self.gradient_penalty, -1., 1.)
+
 
             discriminator_loss_orig = -self.real_labels_loss + self.fake_labels_loss
             discriminator_loss = discriminator_loss_orig + 10. * self.gradient_penalty
@@ -156,7 +160,7 @@ class Model(object):
             pd_loss = self.real_labels_loss - self.fake_labels_loss
             # pd_loss = -self.fake_labels_loss
 
-        loss = rl_loss
+        loss = 5. * rl_loss
         if self.disc_coeff > 0.:
             loss +=  self.TRAIN_GEN * self.disc_coeff * pd_loss
 
@@ -284,7 +288,7 @@ class Model(object):
             self.CLIPRANGE : cliprange,
             self.OLDNEGLOGPAC : neglogpacs,
             self.OLDVPRED : values,
-            self.TRAIN_GEN : self.training_i % 5 == 0,
+            self.TRAIN_GEN : self.training_i % 100 == 0,
         }
 
         if self.disc_coeff == 0.0:
