fce739b5303d3ab0ee285b29d4004fbc2eb25c78
Josh Roy <joshnroy@gmail.com>
2020-04-30 15:35:27 -0400

    added evaluation

--- baselines/adversarial_ppo2/model.py
+++ baselines/adversarial_ppo2/model.py
@@ -177,6 +177,7 @@ class Model(object):
         self.train_model = train_model
         self.act_model = act_model
         self.step = act_model.step
+        self.step_evaluate = act_model.step_evaluate
         self.value = act_model.value
         self.initial_state = act_model.initial_state
 
